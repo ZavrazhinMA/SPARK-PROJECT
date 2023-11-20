8c76aab33506f1da6e6f8eafeabe5b93670b39b9
MAKSIM <zavr1987@mail.ru>
2023-11-20 11:11:37 +0300

    base added

---
+++ hh_kafka_consumer/project/Dependencies.scala
@@ -0,0 +1,19 @@
+import sbt._
+object Dependencies {
+  val library: Object {
+    val sparkSql: ModuleID
+    val sparkStreaming: ModuleID
+    val sparkKafka: ModuleID
+
+  } = new {
+    object Version {
+      lazy val spark = "3.5.0"
+    }
+
+    val sparkSql = "org.apache.spark" %% "spark-sql" % Version.spark
+    val sparkStreaming =
+      "org.apache.spark" % "spark-streaming_2.12" % Version.spark
+    val sparkKafka =
+      "org.apache.spark" % "spark-sql-kafka-0-10_2.12" % Version.spark
+  }
+}
